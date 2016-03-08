38680b89964a290aaac5dc7023a363885359c597
Andrea Rizzini <andrea_rizzini@mastercard.com>
2016-03-08 10:48:57 +0000

    arizzini: committing code to double check if the response has content before trying to convert it to a map.

--- MasterCard/Core/ApiController.php
+++ MasterCard/Core/ApiController.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Copyright 2016 MasterCard International.
  *
@@ -28,24 +29,19 @@
 
 namespace MasterCard\Core;
 
-
 use MasterCard\Core\Exception\ApiException;
 use MasterCard\Core\Exception\InvalidRequestException;
 use MasterCard\Core\Exception\AuthenticationException;
 use MasterCard\Core\Exception\ObjectNotFoundException;
 use MasterCard\Core\Exception\NotAllowedException;
 use MasterCard\Core\Exception\SystemException;
-
-
 use MasterCard\Core\ApiConfig;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
+class ApiController {
 
-class ApiController
-{
-    
     const HTTP_SUCCESS = 200;
     const HTTP_NO_CONTENT = 204;
     const HTTP_AMBIGUOUS = 300;
@@ -54,7 +50,7 @@ class ApiController
     const HTTP_NOT_FOUND = 404;
     const HTTP_NOT_ALLOWED = 405;
     const HTTP_BAD_REQUEST = 400;
-    
+
     /**
      * @ignore
      */
@@ -65,125 +61,111 @@ class ApiController
         'show' => 'GET',
         'update' => 'PUT'
     );
-
-  
     public $fullUrl = null;
     public $baseUrl = null;
 
     function __construct($basePath) {
-        
+
         $this->checkState();
-        
-        if ($basePath == null || trim($basePath) == '' ||  strlen($basePath) == 0)
-        {
-            throw new ApiException ("basePath cannot be empty");
+
+        if ($basePath == null || trim($basePath) == '' || strlen($basePath) == 0) {
+            throw new ApiException("basePath cannot be empty");
         }
-        
-        $baseUrl =  ApiConfig::API_BASE_LIVE_URL;
+
+        $baseUrl = ApiConfig::API_BASE_LIVE_URL;
         if (ApiConfig::isSandbox()) {
             $baseUrl = ApiConfig::API_BASE_SANDBOX_URL;
         }
-        
-        $fullUrl = $baseUrl.$basePath;
-        if (filter_var($fullUrl, FILTER_VALIDATE_URL) == FALSE)
-        {
-            throw new ApiException ("fullUrl: '".$fullUrl."' is not a valid url");
+
+        $fullUrl = $baseUrl . $basePath;
+        if (filter_var($fullUrl, FILTER_VALIDATE_URL) == FALSE) {
+            throw new ApiException("fullUrl: '" . $fullUrl . "' is not a valid url");
         }
-        
+
         $this->fullUrl = $fullUrl;
         $this->baseUrl = Util::normalizeUrl($fullUrl);
-        
-        
     }
-    
-    
+
     /// <summary>
     /// Checks the state.
     /// </summary>
-    private function checkState ()
-    {
+    private function checkState() {
 
-        if (ApiConfig::getAuthentication() == null) 
-        {
-            throw new ApiException ("No ApiConfig::authentication has been configured");
+        if (ApiConfig::getAuthentication() == null) {
+            throw new ApiException("No ApiConfig::authentication has been configured");
         }
 
-        if (filter_var(ApiConfig::API_BASE_LIVE_URL, FILTER_VALIDATE_URL) == FALSE) 
-        {
+        if (filter_var(ApiConfig::API_BASE_LIVE_URL, FILTER_VALIDATE_URL) == FALSE) {
             throw new ApiException("Invalid URL supplied for API_BASE_LIVE_URL");
         }
 
 
-        if (filter_var(ApiConfig::API_BASE_SANDBOX_URL, FILTER_VALIDATE_URL) == FALSE) 
-        {
+        if (filter_var(ApiConfig::API_BASE_SANDBOX_URL, FILTER_VALIDATE_URL) == FALSE) {
             throw new ApiException("Invalid URL supplied for API_BASE_SANDBOX_URL");
         }
     }
-        
-    private function removeForwardSlashFromTail($url)
-    {
-       return preg_replace('{/$}', '', $url);
+
+    private function removeForwardSlashFromTail($url) {
+        return preg_replace('{/$}', '', $url);
     }
-    
-    private function appendToQueryString ($s, $stringToAppend)
-    {
+
+    private function appendToQueryString($s, $stringToAppend) {
         if (strpos($s, "?") == false) {
             $s .= "?";
         } else {
             $s .= "&";
         }
-             
+
         $s .= $stringToAppend;
-        
+
         return $s;
     }
 
     /**
      * @ignore
      */
-    public function getUrl($type, $action, $baseObjectInstance)
-    {
+    public function getUrl($type, $action, $baseObjectInstance) {
 
         $queryParams = array();
         $url = "";
-        
+
         $url .= "%s/%s";
         array_push($queryParams, $this->removeForwardSlashFromTail($this->fullUrl));
-        
+
         $modifiedType = str_replace("{id}", "", $type);
         array_push($queryParams, $this->removeForwardSlashFromTail($modifiedType));
-        
+
         switch ($action) {
             case "create":
                 break;
             case "read":
             case "update":
             case "delete":
-                if ($baseObjectInstance->containsKey("id")){
+                if ($baseObjectInstance->containsKey("id")) {
                     $url .= "/%s";
                     array_push($queryParams, $baseObjectInstance->get("id"));
                 }
                 break;
             case "list":
                 if ($baseObjectInstance != null && $baseObjectInstance->size() > 0) {
-                    
+
                     //arizzini: addding max
-                    if ($baseObjectInstance->containsKey ("max")) {
-                        $url = $this->appendToQueryString ($url, "max=%s");
+                    if ($baseObjectInstance->containsKey("max")) {
+                        $url = $this->appendToQueryString($url, "max=%s");
                         array_push($queryParams, Util::urlEncode($baseObjectInstance->get("max")));
                     }
 
-                    if ($baseObjectInstance->containsKey ("offset")) {
-                        $url = $this->appendToQueryString  ($url, "offset=%s");
+                    if ($baseObjectInstance->containsKey("offset")) {
+                        $url = $this->appendToQueryString($url, "offset=%s");
                         array_push($queryParams, Util::urlEncode($baseObjectInstance->get("offset")));
                     }
-                    
+
                     if ($baseObjectInstance->containsKey("sorting")) {
                         if (is_array($baseObjectInstanceMap->get("sorting"))) {
                             foreach ($baseObjectInstanceMap->get("sorting") as $key => $value) {
-                                 $url = $this->appendToQueryString  ($url, "sorting[%s]=%s");
-                                 array_push($queryParams, Util::urlEncode($key));
-                                 array_push($queryParams, Util::urlEncode($value));
+                                $url = $this->appendToQueryString($url, "sorting[%s]=%s");
+                                array_push($queryParams, Util::urlEncode($key));
+                                array_push($queryParams, Util::urlEncode($value));
                             }
                         }
                     }
@@ -191,9 +173,9 @@ class ApiController
                     if ($baseObjectInstance->containsKey("filter")) {
                         if (is_array($baseObjectInstanceMap->get("filter"))) {
                             foreach ($baseObjectInstanceMap->get("filter") as $key => $value) {
-                                 $url = $this->appendToQueryString  ($url, "filter[%s]=%s");
-                                 array_push($queryParams, Util::urlEncode($key));
-                                 array_push($queryParams, Util::urlEncode($value));
+                                $url = $this->appendToQueryString($url, "filter[%s]=%s");
+                                array_push($queryParams, Util::urlEncode($key));
+                                array_push($queryParams, Util::urlEncode($value));
                             }
                         }
                     }
@@ -210,80 +192,75 @@ class ApiController
             default:
                 break;
         }
-        
-        $url = $this->appendToQueryString  ($url, "Format=JSON");
+
+        $url = $this->appendToQueryString($url, "Format=JSON");
         $url = vsprintf($url, $queryParams);
-        
+
         return $url;
     }
-    
-    
-    
-    public function getRequest($url, $action, $baseObjectInstance)
-    {
+
+    public function getRequest($url, $action, $baseObjectInstance) {
         $request = null;
-                
+
         switch ($action) {
-        case "create":
+            case "create":
                 $request = new Request("POST", $url, [], json_encode($baseObjectInstance->getProperties()));
                 break;
-        case "delete":
+            case "delete":
                 $request = new Request("DELETE", $url);
                 break;
-        case "update":
+            case "update":
                 $request = new Request("PUT", $url, [], json_encode($baseObjectInstance->getProperties()));
                 break;
-        case "read":
+            case "read":
                 $request = new Request("GET", $url);
                 break;
-        case "list":
+            case "list":
                 $request = new Request("GET", $url);
                 break;
         }
-        
-        $request =  $request->withHeader ("Accept", "application/json");
-        $request =  $request->withHeader ("Content-Type", "application/json");
-        $request =  $request->withHeader ("User-Agent", "Java-SDK/".ApiConfig::VERSION);
+
+        $request = $request->withHeader("Accept", "application/json");
+        $request = $request->withHeader("Content-Type", "application/json");
+        $request = $request->withHeader("User-Agent", "Java-SDK/" . ApiConfig::VERSION);
         $request = ApiConfig::getAuthentication()->signRequest($url, $request);
-        
+
         return $request;
     }
 
-    
-    public function execure($type, $action, $baseObjectInstance)
-    {
+    public function execure($type, $action, $baseObjectInstance) {
         $url = $this->getUrl($type, $action, $baseObjectInstance);
         $request = $this->getRequest($url, $action, $baseObjectInstance);
-        
-        
+
+
         try {
             $client = new Client();
             $response = $client->send($request);
             $statusCode = $response->getStatusCode();
-            if ($statusCode < self::HTTP_AMBIGUOUS)
-            {
-                return json_decode($response->getBody()->getContents(), true);
+            if ($statusCode < self::HTTP_AMBIGUOUS) {
+                $responseContent = $response->getBody()->getContents();
+                if (strlen($responseContent) > 0) {
+                    return json_decode($response->getBody()->getContents(), true);
+                } else {
+                    return array();
+                }
             } else {
                 $this->handleException($response);
             }
         } catch (RequestException $ex) {
-            if ($ex->hasResponse())
-            {
+            if ($ex->hasResponse()) {
                 $this->handleException($ex->getResponse());
             } else {
                 throw new SystemException("An unexpected error has been raised.  Looks like there's something wrong at our end.");
             }
         }
     }
-    
-    
-    private function handleException($response)
-    {
+
+    private function handleException($response) {
         $status = $response->getStatusCode();
         $bodyArray = json_decode($response->getBody()->getContents());
-        
-        if ($status < 500)
-        {
+
+        if ($status < 500) {
             switch ($status) {
                 case self::HTTP_BAD_REQUEST:
                     throw new InvalidRequestException("Bad request", $status, $bodyArray);
@@ -305,13 +282,8 @@ class ApiController
                     break;
             }
         } else {
-            throw new SystemException("An unexpected error has been raised.  Looks like there's something wrong at our end." , $status, $bodyMessage);
+            throw new SystemException("An unexpected error has been raised.  Looks like there's something wrong at our end.", $status, $bodyMessage);
         }
-        
-
-        
     }
 
-
 }
-
