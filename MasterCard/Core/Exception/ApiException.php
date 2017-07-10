1d1d6842156d034dd98cb94202002a66c872835a
Andrea Rizzini <andrea_rizzini@mastercard.com>
2017-07-10 11:19:24 +0100

    arizzini: better handling of list of errors.

--- MasterCard/Core/Exception/ApiException.php
+++ MasterCard/Core/Exception/ApiException.php
@@ -39,10 +39,13 @@ class ApiException extends \Exception
 {
 
     protected $message;
-    protected $rawErrorData;
+    
     protected $httpStatus;
     protected $reasonCode;
     protected $source;
+    
+    protected $rawErrorData;
+    protected $error;
     protected $errors;
     
     private $http_status_codes = array(100 => "Continue", 101 => "Switching Protocols", 102 => "Processing", 200 => "OK", 201 => "Created", 202 => "Accepted", 203 => "Non-Authoritative Information", 204 => "No Content", 205 => "Reset Content", 206 => "Partial Content", 207 => "Multi-Status", 300 => "Multiple Choices", 301 => "Moved Permanently", 302 => "Found", 303 => "See Other", 304 => "Not Modified", 305 => "Use Proxy", 306 => "(Unused)", 307 => "Temporary Redirect", 308 => "Permanent Redirect", 400 => "Bad Request", 401 => "Unauthorized", 402 => "Payment Required", 403 => "Forbidden", 404 => "Not Found", 405 => "Method Not Allowed", 406 => "Not Acceptable", 407 => "Proxy Authentication Required", 408 => "Request Timeout", 409 => "Conflict", 410 => "Gone", 411 => "Length Required", 412 => "Precondition Failed", 413 => "Request Entity Too Large", 414 => "Request-URI Too Long", 415 => "Unsupported Media Type", 416 => "Requested Range Not Satisfiable", 417 => "Expectation Failed", 418 => "I'm a teapot", 419 => "Authentication Timeout", 420 => "Enhance Your Calm", 422 => "Unprocessable Entity", 423 => "Locked", 424 => "Failed Dependency", 424 => "Method Failure", 425 => "Unordered Collection", 426 => "Upgrade Required", 428 => "Precondition Required", 429 => "Too Many Requests", 431 => "Request Header Fields Too Large", 444 => "No Response", 449 => "Retry With", 450 => "Blocked by Windows Parental Controls", 451 => "Unavailable For Legal Reasons", 494 => "Request Header Too Large", 495 => "Cert Error", 496 => "No Cert", 497 => "HTTP to HTTPS", 499 => "Client Closed Request", 500 => "Internal Server Error", 501 => "Not Implemented", 502 => "Bad Gateway", 503 => "Service Unavailable", 504 => "Gateway Timeout", 505 => "HTTP Version Not Supported", 506 => "Variant Also Negotiates", 507 => "Insufficient Storage", 508 => "Loop Detected", 509 => "Bandwidth Limit Exceeded", 510 => "Not Extended", 511 => "Network Authentication Required", 598 => "Network read timeout error", 599 => "Network connect timeout error");
@@ -60,19 +63,21 @@ class ApiException extends \Exception
             $this->message = $message;
         }
                 
+        
         $this->httpStatus = $status;
         $this->reasonCode = null;
         $this->source = null; 
+        
+        $this->rawErrorData = $errorData;
         $this->errors = array();
+        $this->error = null;
         
         $this->parseErrors($errorData);
         $this->parseError(0);
 
         
     }
-    
 
-    
 
     public function getErrorSize() {
         return count($this->errors);
@@ -80,9 +85,16 @@ class ApiException extends \Exception
     
     public function parseError($index) {
         if (!empty($this->errors) && $index >=0 && $index < $this->getErrorSize()) {
-            $this->parseErrorMap($this->errors[$index]);
+            $this->error = $this->errors[$index];
+            $this->parseErrorMap();
         }
     }
+    
+    public function getError() {
+        return $this->error;
+    }
+    
+
     
     /**
      * Returns a map of all error data returned by the API.
@@ -138,6 +150,7 @@ class ApiException extends \Exception
             
             
             $errors = array();
+            //arizzini: this takes care of the list returned in the response
             if ($this->isAssoc($errorData)) {
                 $errors[] = $errorData;
             } else if (is_array ($errorData)){
@@ -146,7 +159,8 @@ class ApiException extends \Exception
                 $errors[] = $errorData;
             }
             
-            
+            //arizzini: then we parse each invididual error into a 
+            //          case insenstive map.
             foreach ($errors as $error) {
                 $smartMap = new CaseInsensitiveMap();
                 $smartMap->setAll($error);
@@ -163,16 +177,13 @@ class ApiException extends \Exception
                 } else if ($smartMap->containsKey("reasoncode")) {
 //                    echo "contains reasoncode";
                     $this->addError($smartMap->getBaseMapAsArray());
-                } else {
-                    $this->rawErrorData = $smartMap;
-                }
+                } 
             }
         }
     }
     
     
     private function addError($error) {
-        
         if ($this->isAssoc($error)) {
             $map = new CaseInsensitiveMap();
             $map->setAll($error);
@@ -186,19 +197,18 @@ class ApiException extends \Exception
         }  
     }
     
-    private function parseErrorMap($smartMap) {
-        $this->rawErrorData = $smartMap;
-        if ($smartMap->containsKey('description'))
+    private function parseErrorMap() {
+        if ($this->error->containsKey('description'))
         {
-            $this->message = $smartMap->get('description');
+            $this->message = $this->error->get('description');
         }
-        if ($smartMap->containsKey('reasoncode'))
+        if ($this->error->containsKey('reasoncode'))
         {
-            $this->reasonCode = $smartMap->get('reasoncode');
+            $this->reasonCode = $this->error->get('reasoncode');
         }
-        if ($smartMap->containsKey('source'))
+        if ($this->error->containsKey('source'))
         {
-            $this->source = $smartMap->get('source');
+            $this->source = $this->error->get('source');
         }
     }
     
