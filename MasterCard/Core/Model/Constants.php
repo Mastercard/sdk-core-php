7775269b327ddd36546cbdb9badbf450293cf6a2
Andrea Rizzini <andrea.rizzini@mastercard.com>
2018-01-31 10:09:38 +0000

    arizzini: bumping up version to 1.4.7

--- MasterCard/Core/Model/Constants.php
+++ MasterCard/Core/Model/Constants.php
@@ -32,7 +32,7 @@ namespace MasterCard\Core\Model;
 
 class Constants {
     const SDK = "mastercard-api-core(php)";
-    const VERSION = "1.4.6"; 
+    const VERSION = "1.4.7"; 
     
     public static function getCoreVersion() {
         return Constants::SDK.":".Constants::VERSION;
